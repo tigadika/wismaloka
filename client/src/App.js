9396f331d4b50c501c6b047fe2bc3e67abcd6fcd
tigadika <amardika.noor@gmail.com>
2022-05-21 23:18:20 +0700

    hapus app mantap

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,38 @@
-import logo from './logo.svg';
+import logo from "./logo.svg";
 import { Routes, Route, Link } from "react-router-dom";
-import LoginPage from './views/LoginPage';
-import HomePage from './views/HomePage';
-import CreatePage from './views/CreatePage';
-import { BlockAccessToLogin, AccessOpen } from './component/RouterGuard';
-
+import LoginPage from "./views/LoginPage";
+import HomePage from "./views/HomePage";
+import CreatePage from "./views/CreatePage";
+import { BlockAccessToLogin, AccessOpen } from "./component/RouterGuard";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<BlockAccessToLogin><HomePage /></BlockAccessToLogin>} />
-        <Route path="/login" element={<AccessOpen><LoginPage /></AccessOpen>} />
-        <Route path="/addNewPost" element={<BlockAccessToLogin><CreatePage /></BlockAccessToLogin> } />
+        <Route
+          path="/"
+          element={
+            <BlockAccessToLogin>
+              <HomePage />
+            </BlockAccessToLogin>
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <AccessOpen>
+              <LoginPage />
+            </AccessOpen>
+          }
+        />
+        <Route
+          path="/addNewPost"
+          element={
+            <BlockAccessToLogin>
+              <CreatePage />
+            </BlockAccessToLogin>
+          }
+        />
       </Routes>
     </div>
   );
