e49ac775e0704abf5893caa621d4235145b9cd2a
bayupangestu <bayupangestu21@gmail.com>
2022-05-23 21:55:24 +0700

    Maps and Detail Page done

--- client-user/src/views/LocationSearch.js
+++ client-user/src/views/LocationSearch.js
@@ -1,7 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useQuery } from "@apollo/client";
+import { GET_HOUSE } from "../queries/houseQuery";
+import MapSearch from "../components/MapSearch";
 
 export default function LocationSearch() {
+  const { loading, error, data } = useQuery(GET_HOUSE);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
   return (
     <>
       <div className="flex flex-row justify-around">
@@ -10,20 +16,16 @@ export default function LocationSearch() {
           className="w-full py-3 border-b-2 border-r-2  hover:bg-emerald-50 hover:cursor-pointer"
         >
           <div>
-            <p className="text-emerald-700 text-lg font-bold">
-              Based on budget
-            </p>
+            <p className="text-emerald-700 text-lg font-bold">Based on budget</p>
           </div>
         </Link>
         <div className="w-full py-3 border-b-4 border-b-emerald-700 hover:bg-emerald-50 hover:cursor-pointer">
-          <p className="text-emerald-700 text-lg font-bold">
-            Based on location
-          </p>
+          <p className="text-emerald-700 text-lg font-bold">Based on location</p>
         </div>
       </div>
       <div>
         <div className="flex flex-wrap my-6 mx-4 h-20 rounded-lg shadow">
-          Google Maps Container
+          <MapSearch data={data}></MapSearch>
         </div>
         <form>
           <div className="mx-4 mb-6">
